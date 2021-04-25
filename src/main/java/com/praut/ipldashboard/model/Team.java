cb44e1f0af9741f73775451f0de69d38aa5f5b6f
prathamesh.raut <prathamesh.raut@gmail.com>
2021-04-25 14:47:56 +0530

    no arg constructor added

--- src/main/java/com/praut/ipldashboard/model/Team.java
+++ src/main/java/com/praut/ipldashboard/model/Team.java
@@ -1,14 +1,19 @@
 package com.praut.ipldashboard.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Team {
 
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,6 +21,9 @@ public class Team {
 	private String teamName;
 	private long totalMatches;
 	private long totalWins;
+	
+	@Transient
+	private List<Match> matches;
 
 	public Team(String teamName, Long totalMatches) {
 		this.teamName = teamName;
@@ -26,6 +34,4 @@ public class Team {
 	public String toString() {
 		return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
 	}
-	
-	
 }
